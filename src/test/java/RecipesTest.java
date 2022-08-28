ba3d334c7130d3260f6a417e0bf20a55ed0211d8
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2022-08-28 11:59:49 +0200

    Add put delete

--- src/test/java/RecipesTest.java
+++ src/test/java/RecipesTest.java
@@ -1,9 +1,54 @@
+import io.github.geniot.sayagain.gen.model.RecipeDto;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RecipesTest extends TestBase {
+
+    Random random = new Random(System.currentTimeMillis());
+
+    @Before
+    public void setup() {
+        REQUEST.delete("/recipes").then().statusCode(HttpStatus.SC_OK);
+    }
+
     @Test
     public void shouldHaveStatus200ForAllRecipesList() {
-        REQUEST.get("/recipes").then().statusCode(200);
+        REQUEST.get("/recipes").then().statusCode(HttpStatus.SC_OK);
     }
+
+    @Test
+    public void shouldHaveEmptyRecipesList() {
+        REQUEST.get("/recipes").then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("", Matchers.hasSize(0));
+    }
+
+    @Test
+    public void shouldPutDelete() {
+        RecipeDto inRecipeDto = new RecipeDto();
+        inRecipeDto.setTitle(UUID.randomUUID().toString());
+        inRecipeDto.setDescription(UUID.randomUUID().toString());
+        inRecipeDto.setVegetarian(random.nextBoolean());
+        inRecipeDto.setServings(random.nextInt());
+
+        Response createRecipeResponse = REQUEST.body(inRecipeDto).post("/recipes");
+        createRecipeResponse.then().assertThat().statusCode(HttpStatus.SC_CREATED);
+        RecipeDto outRecipeDto = createRecipeResponse.as(RecipeDto.class);
+
+        assertThat(inRecipeDto).usingRecursiveComparison().ignoringFields("id").isEqualTo(outRecipeDto);
+
+        REQUEST.get("/recipes").then().statusCode(200).body("", Matchers.hasSize(1));
+
+
+    }
+
 
 }
